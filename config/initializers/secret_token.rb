59082cf34adedf6dcde8b27a397c7792fefefe39
Michael Hearn <michael.e.hearn@gmail.com>
2011-05-13 14:51:00 -0400

    Initial Commit of stormDispatch

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StormDispatch::Application.config.secret_token = 'c93c8f609e88144ed9283bffa5c89546a63d49f193e4aea7719f50fb6e7d0d408595a2edf3b34bb445a2195583196bcbdaa2c04892e849809d51194153a7ecb0'
