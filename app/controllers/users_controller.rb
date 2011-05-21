fdaa67bc44b2b3d4cce2bc52044b785ace4bef6a
Michael Hearn <michael.e.hearn@gmail.com>
2011-05-20 20:44:40 -0400

    Added editing and updating users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,37 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
-	@title = "#{@user.first_name} #{@user.last_name}"
+    @user  = User.find(params[:id])
+	  @title = "#{@user.first_name} #{@user.last_name}"
   end
   
   def new
-  	@user = User.new
+  	@user  = User.new
     @title = "Sign Up"
   end
   
   def create
     @user = User.new(params[:user])
-	if @user.save
-	  redirect_to @user, :flash => { :success => "Welcome to ramples.com!" }
-	  sign_in @user
-	else
-	  @title = "Sign Up"
-	  render 'new'
-	end
+	  if @user.save
+	    redirect_to @user, :flash => { :success => "Welcome to ramples.com!" }
+	    sign_in @user
+    else
+      @title = "Sign Up"
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user  = User.find(params[:id])
+    @title = "Edit User"
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      redirect_to @user, :flash => { :success => "Profile udpated." }
+    else
+      @title = "Edit User"
+      render 'edit'
+    end
   end
 end
